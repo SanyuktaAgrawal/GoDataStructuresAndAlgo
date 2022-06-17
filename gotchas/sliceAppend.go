b4460315bd83910880f032147d7a807bc9a75fe7
Sanyukta Agrawal <sanyuktaagrawal121@gmail.com>
2022-06-17 10:57:12 +0530

    Update sliceAppend.go

--- gotchas/sliceAppend.go
+++ gotchas/sliceAppend.go
@@ -11,19 +11,17 @@ func main() {
 	slice[0] = 1
 	fmt.Println(slice) //[1 0 0]
 
-	slice = append(slice, 4)
-	fmt.Println(slice) // [1 0 0 4]
+	slice = append(slice, 4) // works, new element gets added to the slice
+	fmt.Println(slice)       // [1 0 0 4]
 
-	AddOne(slice)
+	modifySlice(slice)
 	fmt.Println(slice) //[11 0 3 4]
 }
 
-func AddOne(s []int) {
+func modifySlice(s []int) {
 	s[0] = 11
 
-	//when the new elements append to the slice a new array is created.
-	//Now, the elements present in the existing array are copied into a new array and return a new slice reference to this array (new array).
-	s = append(s, 2)
+	s = append(s, 2) // new element gets added to the slice inside function, but not visible to caller function.
 	s[2] = 3
 
 	fmt.Println(s) //[11 0 3 4 2]
